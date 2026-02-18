2d2e71bd60d2fd2a2dd9cf26f50cbff6debc28dc
git-bugra <bugraozer791@gmail.com>
2026-02-18 18:47:22 +0000

    feat(networking): progress bar and OOP
    	-Replace functional programming source code to OOP,
    	-Add progress bar for visuals.

--- networking/handle_datasets.py
+++ networking/handle_datasets.py
@@ -1,43 +1,52 @@
 import json
 import requests
 import pathlib as pl
+from tqdm import tqdm
 
-def load_config(filename):
-    """
-    Load configuration file for file operations.
-    """
-    with open(filename, "r") as f:
-        return json.load(f)
+class DatasetDownloader():
 
-def download_file(url, destination):
-    """
-    Open and write dataset.
-    """
-    response=requests.get(url, stream=True)
-    if response.status_code == 200 and destination.exists():
-        with open(destination, "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
-            return True
-    else:
-        raise Exception(f'Download failed. Server threw {response.status_code}.')
+    def __init__(self):
+        self.json='dataset.json'
+        self.response=None
 
-def print_progress():
-    """
+    def _load_config(self):
+        """
+        Load configuration file for file operations.
+        """
+        try:
+            with open(self.json, "r") as f:
+                return json.load(f)
+        except ValueError:
+            raise Exception('Failed to open .json config.')
+        except FileNotFoundError:
+            raise Exception('Failed to find .json config.')
 
-    """
+    def _download_file(self, url, destination, stream=True):
+        """
+        Open and write dataset.
+        """
+        self.response=requests.get(url, stream=stream)
+        total_chunks=(float(self.response.headers.get('Content-Length'))+8191)//8192
+        if self.response.status_code == 200:
+            with open(destination, "wb") as f:
+                for chunk in tqdm(self.response.iter_content(chunk_size=8192), total=total_chunks, unit='B', unit_scale=True, bar_format='\033[37m{l_bar}\033[32m{bar}\033[37m{r_bar}', ncols=120, desc=f'downloading datasets'):
+                    f.write(chunk)
+                return True
+        else:
+            raise Exception(f'Download failed. Server threw status code: {self.response.status_code}.')
 
-def fetch_imdb_dataset():
-    """
-    Orchestrate full file operation.
-    """
-    requests_config = load_config('dataset.json')
-    folder = requests_config['download_dir']
-    for dataset in requests_config['datasets']:
-        url = dataset['url']
-        filename = dataset['filename']
-        destination = pl.Path(__file__).parent.parent / folder / filename
-        download_file(url, destination)
+    def fetch_imdb_dataset(self):
+        """
+        Orchestrate full file operation.
+        """
+        requests_config=self._load_config()
+        folder=requests_config['download_dir']
+        for dataset in requests_config['datasets']:
+            url=dataset['url']
+            filename=dataset['filename']
+            destination=pl.Path(__file__).parent.parent / folder / filename
+            self._download_file(url, destination)
 
 if __name__ == '__main__':
-    fetch_imdb_dataset()
+    dataset_obj=DatasetDownloader()
+    dataset_obj.fetch_imdb_dataset()
